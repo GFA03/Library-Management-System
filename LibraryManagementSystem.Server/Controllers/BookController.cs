456add38062d6016d79ff413feaba32f6e27e4ab
Gavrila Alexandru <florinalexandru302003@gmail.com>
2024-01-23 10:34:08 +0200

    Added "Book" model

---
+++ LibraryManagementSystem.Server/Controllers/BookController.cs
@@ -0,0 +1,31 @@
+﻿using LibraryManagementSystem.Server.Data.Models;
+using LibraryManagementSystem.Server.Repositories;
+using Microsoft.AspNetCore.Mvc;
+
+namespace LibraryManagementSystem.Server.Controllers
+{
+    [ApiController]
+    [Route("api/books")]
+    public class BookController : ControllerBase
+    {
+        private readonly BookRepository _bookRepository;
+        public BookController(BookRepository bookRepository)
+        {
+            _bookRepository = bookRepository;
+        }
+
+        [HttpGet]
+        public IActionResult GetAllBooks()
+        {
+            var books = _bookRepository.GetAllBooks();
+            return Ok(books);
+        }
+
+        [HttpPost]
+        public IActionResult AddBook(Book book)
+        {
+            _bookRepository.AddBook(book);
+            return CreatedAtAction(nameof(GetAllBooks), null);
+        }
+    }
+}
---
+++ LibraryManagementSystem.Server/Repositories/BookRepository.cs
@@ -0,0 +1,19 @@
+﻿using LibraryManagementSystem.Server.Data.Models;
+
+namespace LibraryManagementSystem.Server.Repositories
+{
+    public class BookRepository
+    {
+        private readonly List<Book> _books = new List<Book>();
+
+        public List<Book> GetAllBooks()
+        {
+            return _books;
+        }
+
+        public void AddBook(Book book)
+        {
+            _books.Add(book);
+        }
+    }
+}
